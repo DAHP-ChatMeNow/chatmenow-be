201dd4d90bf8c83a0c7bfd59bf7e7962a01e1b93
Lê Nguyễn Quỳnh <shibalnq2112@gmail.com>
2026-04-12 21:12:19 +0700

    thêm tính năng admin quản lý bạn bè

--- src/api/routes/admin.route.js
+++ src/api/routes/admin.route.js
@@ -5,4 +5,7 @@ const { verifyToken, requireAdmin } = require("../middleware/authMiddleware");
 
 router.get("/stats", verifyToken, requireAdmin, adminController.getStats);
 
+// Quản lý bạn bè của người dùng (Admin)
+router.delete("/users/:userId/friends/:friendId", verifyToken, requireAdmin, adminController.removeUserFriend);
+
 module.exports = router;
